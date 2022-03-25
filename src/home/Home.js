e110d679448b9c530a44eb045340ce32c4298a67
chry8822 <goggg8822@naver.com>
2022-03-25 16:18:37 +0900

    moblie swiper test

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,4 +1,4 @@
-import { Component, useContext, useState } from 'react';
+import { Component, useContext, useState, useCallback, useEffect } from 'react';
 import Navbar from "../component/navbar/Navbar"
 import Featured from './../component/featured/Featured';
 import List from './../component/list/List';
@@ -10,14 +10,34 @@ import { dataContext } from '../App';
 import ListModal from '../component/ListModal/ListModal';
 import "./home.scss"
 import Modal from '../component/Modal/Modal';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-export default function Home(props) {
+import 'swiper/css';
+
+export default function Home(props) { 
   let infoData = useContext(dataContext);
   const [modalIndex, setModalIndex] = useState(-1);
+  const [isMobileView, setIsMobileView] = useState(false);
 
   const handleClickListItem = (itemIndex) => {
     setModalIndex(itemIndex);
   }
+
+  const onResize = useCallback(() => {
+    console.log('onResize');
+    setIsMobileView(window.innerWidth < 680);
+  }, [])
+
+  useEffect(() => {
+    onResize();
+    window.addEventListener('resize', onResize);
+    return () => {
+      window.removeEventListener('resize', onResize);
+    }
+  }, []);
+
+  console.log('isMobileView', isMobileView);
+
   return (
     <div className="home">
       <button onClick={props.pageToHome} className="moveTop">
@@ -27,16 +47,38 @@ export default function Home(props) {
       <Featured />
 
       <List title="Project" eduList="list">
-          {
+        {
+          isMobileView && (
+            <Swiper
+              spaceBetween={10}
+              slidesPerView={1}>
+            {
+              infoData.map((data, index) =>
+                (
+                  <SwiperSlide>
+                    <ListItem data={data} key={`${index}`} index={index} handleClickListItem={() => { handleClickListItem(index) }} />
+                    {/* {index} */}
+                  </SwiperSlide>
+                )
+              )
+            }
+            </Swiper>
+          )
+        }
+        {
+          !isMobileView && 
             infoData.map((data, index) =>
-              (<ListItem data={data} key={`${index}`} index={index} handleClickListItem={() => { handleClickListItem(index) }} />)
+              (
+                <ListItem data={data} key={`${index}`} index={index} handleClickListItem={() => { handleClickListItem(index) }} />
+              )
             )
-          }
+        }
       </List>
 
       <List title="Education" eduList="eduList list">
           {
             eduData.map((data, index) => (
+              
               <EducationListItem data={data} key={`${index}`} index={index} />
             ))
           }
